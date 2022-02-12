d88c3230f37e55053c1be3c27f29f97a99d73097
Dominic Verrichia <91326454+verrichd@users.noreply.github.com>
2022-02-12 11:08:42 -0500

    Finished CentralTendency

--- ProbAndStats/src/notes/CentralTendency.java
+++ ProbAndStats/src/notes/CentralTendency.java
@@ -2,7 +2,18 @@
 package notes;
 import java.util.Random;
 import java.util.Arrays;
-
+/**
+ * CentralTendency has methods that return the statistical measurements
+ * of central tendency including mean, median, mode, and standard deviation
+ * 
+ * There are two constructors for this object, one that accepts an integer to determine the size
+ * of a then randomly filled array of integers and the other accepts a user generated integer array
+ * 
+ * The method printNums is used in the random array constructor to provide context for the calculations
+ * 
+ * @author DominicVerrichia
+ *
+ */
 public class CentralTendency {
 	private int[] nums;
 	
@@ -21,6 +32,22 @@ public class CentralTendency {
 		nums = newNums;
 	}
 	
+	public void printNums() {
+		System.out.print("Numbers in the array: {");
+		for(int i = 0; i < nums.length; i++) {
+			if(i == nums.length - 1) {
+				System.out.println(nums[i] + "}");
+			}else {
+			System.out.print(nums[i] + ",");
+			}
+		}
+	}
+	
+	/**
+	 * mean calculates the typical average of the integer array
+	 * by adding every element and dividing by the amount of elements
+	 * @return
+	 */
 	public double mean() {
 		double sum = 0;
 		for(int i : nums) {
@@ -29,66 +56,31 @@ public class CentralTendency {
 		return sum/nums.length;
 	}
 	
-	public double mean(int[] numbers) {
-		double sum = 0;
-		for(int i : numbers) {
-		 sum = sum + i;
-		}
-		return sum/numbers.length;
-	}
-	
-	public int[] sortNums(int[] numbers) {
-		 int min = numbers[0];
-		 int minIndex = 0;
-		for(int j = 0; j < numbers.length; j++) {
-			for(int i = 0; i < numbers.length; i++) {
-				if(numbers[i] < min) {
-				 min = numbers[i];
-				 minIndex = i;
-				}
-			}
-		   int temp = numbers[j];
-		   numbers[j] = numbers[minIndex];
-		   numbers[minIndex] = temp; 
-		   
-		}
-		return numbers;
-	 }
-	 
+	 /**
+	  * median will return the middle element in an array of odd length
+	  * but will return the average of the middle two values in an even 
+	  * length array
+	  * 
+	  * @return middle value of the integer array
+	  */
 	public double median() {
-		/*int targetIndex = nums.length/2;
-		int[] sorted = new int[nums.length/2];
-		if(nums.length % 2 == 1) {
-			for(int i = 0; i < targetIndex; i++) {
-				int smallest = nums[0];
-				for(int j = i; j < nums.length; j++) {
-					
-				}
-			}
-		}else {
-		
-		}
-		
-		int[] middleTwo = {nums[nums.length/2],nums[(nums.length/2) - 1]};
-		return mean(middleTwo);
-		*/
 		Arrays.sort(nums);
 		if(nums.length % 2 == 1) {
 			return nums[nums.length/2];
 		}
+		double median = nums[nums.length/2] + nums[(nums.length/2) - 1] / 2;
 		
-		int[] middleTwo = {nums[nums.length/2],nums[(nums.length/2) - 1]};
-		return mean(middleTwo);
+		return median;
 		
 	}
 	
-	
-	public void printNums() {
-		for(int i = 0; i < nums.length; i++) {
-			System.out.println(nums[i]);
-		}
-	}
-	
+	/**
+	 * mode will return the most occurring number in the array 
+	 * and will throw an exception when there is not one singular number 
+	 * that occurs more than the rest
+	 * @return the mode of an integer array
+	 * @throws Exception when there is no mode
+	 */
 	public int mode() throws Exception {
 		Exception noMode = new Exception("No Mode");
 		boolean newMode = false;
@@ -118,21 +110,12 @@ public class CentralTendency {
 		return mode;
 	}
 	
-	public double getVar(int[] numbers) {
-		int n = numbers.length;
-		double var = 0;
-		double average = 0;
-		for(int i = 0; i < n; i++) {
-			average += numbers[i];
-		}
-		average = average/n;
-		for(int i = 0; i < n; i++) {
-		var = var + (numbers[i] - average)*(numbers[i] - average);
-		}
-		
-		return var/(numbers.length-1);
-	}
-	
+	/**
+	 * variance returns the variance of an integer set with internal 
+	 * use of the mean method to calculate the sum of the squares of
+	 * each element's distance from the average
+	 * @return variance as described by the statistical method
+	 */
 	public double getVar() {
 		int n = nums.length;
 		double var = 0;
@@ -141,16 +124,22 @@ public class CentralTendency {
 		var = var + (nums[i] - average)*(nums[i] - average);
 		}
 		
-		return var/nums.length-1;
+		return var/(nums.length-1);
 	}
 	
+	/**
+	 * getSTDev returns the standard deviation of the integer array
+	 * with use of the variance method. This method takes advantage of 
+	 * the relationship between variance and standard deviation 
+	 * 
+	 *  That is variance = (standard deviation)^2
+	 *  
+	 * @return standard deviation by calculating variance then taking square root
+	 */
 	public double getSTDev() {
 		return Math.pow(getVar(),0.5);
 	}
 	
-	public double getSTDev(int[] numbers) {
-		return Math.pow(getVar(numbers),0.5);
-	}
 	
 	
 }
