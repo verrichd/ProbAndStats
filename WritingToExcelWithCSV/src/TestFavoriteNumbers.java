eef29c787d7e0e7976b418bdf72c59fe53ab5afb
Dominic Verrichia <91326454+verrichd@users.noreply.github.com>
2022-03-21 14:37:28 -0400

    Delete WritingToExcelWithCSV directory

--- WritingToExcelWithCSV/src/TestFavoriteNumbers.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-public class TestFavoriteNumbers {
-
-	public static void main(String[] args) throws IOException {
-		 FavoriteNumbers fn = new FavoriteNumbers(1000);
-		 fn.assignNumbers();
-	}
-}
